453acd32604aa144d474f9d5e1edde9c516e8f70
MartinDolenc <martin.dolenc7@gmail.com>
2018-04-24 20:41:44 +0200

    Dodal metodo za preverjanje pogojev za zmago. Dopolnil metodo za odigrano potezo tako da lahko dobimo neodločen rezultat.

--- Spin/NeumannM16$/_System/Desktop/SnowflipperPenguin/Gomoku/src/Igra.java
+++ Spin/NeumannM16$/_System/Desktop/SnowflipperPenguin/Gomoku/src/Igra.java
@@ -15,7 +15,7 @@ public class Igra {
 	private Igralec na_potezi;
 	
 	// Ustvarimo novo igralno plošèo dimenzije N x N.
-	// z i oznaèimo vrstice, z j pa stolpce.
+	// z i oznaèimo stolpce, z j pa vrstice.
 	
 	public void zaèetek_igre() {
 		igralna_plošèa = new Polje[N][N];
@@ -33,11 +33,26 @@ public class Igra {
 		if (igralna_plošèa[p.getX()][p.getY()] == Polje.Prazno) {
 			igralna_plošèa[p.getX()][p.getY()] = na_potezi.getPolje();
 			
+			
+			
 			if(aliJeKdoZmagal(p)) {
 				System.out.println("GJ zmagu si");
-			}else {
+			} else {
 			na_potezi = na_potezi.nasprotnik();
 			}
+			
+			// Pogledamo ali obstaja še kaka poteza
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(igralna_plošèa[i][j] == Polje.Prazno) {
+						break;
+					} else {
+						System.out.println("Igra se je konèala neodloèeno");
+					}
+				}
+			}
+			
 			return true;
 		}
 		else {
@@ -49,9 +64,109 @@ public class Igra {
 	
 	public boolean aliJeKdoZmagal(Poteza p) {
 		
-		// definiri smeri v kater je treba pregledat (8 smeri) in se zapelji èez nje in preglej do M (5) koordinat.
+		int S = 0; // Koliko smo jih že našli v vrsti/stoplcu/diagonali
+		
+		// vektorji (0, 1), (1, 1), (1, 0), (1, -1)
+		
+		// Pregled na X osi
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX() + i][p.getY()] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX() - i][p.getY()] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		if (S >= 5) {
+			return true;
+		} else {
+			S = 0;
+		}
+		
+		// Pregled na premici y = x
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX() + i][p.getY() + i] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX() - i][p.getY() - i] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		if (S >= 5) {
+			return true;
+		} else {
+			S = 0;
+		}
+		
+		// Pregled na y osi
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX()][p.getY() + i] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX()][p.getY() - i] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		if (S >= 5) {
+			return true;
+		} else {
+			S = 0;
+		}
+		
+		// Pregled na premici y = -x
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX() + i][p.getY() - i] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		for (int i = 0; i < M; i++) {
+			if(igralna_plošèa[p.getX() - i][p.getY() + i] == na_potezi.getPolje()) {
+				S++;
+			} else {
+				break;
+			}
+		}
+		
+		if (S >= 5) {
+			return true;
+		} else {
+			S = 0;
+		}
 		
 		return false;
 	}
+	
+	 
 	
 }

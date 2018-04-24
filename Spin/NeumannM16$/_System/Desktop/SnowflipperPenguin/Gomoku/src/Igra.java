38876d0d8bd3fef99a0eca89580626656d8e8ca8
MartinDolenc <martin.dolenc7@gmail.com>
2018-04-24 16:55:27 +0200

    Dodal metodo odigrajPotezo v classu Igra. Dodal metodi nasprotnik in getPolje v classu Igralec.

--- Spin/NeumannM16$/_System/Desktop/SnowflipperPenguin/Gomoku/src/Igra.java
+++ Spin/NeumannM16$/_System/Desktop/SnowflipperPenguin/Gomoku/src/Igra.java
@@ -4,16 +4,21 @@ public class Igra {
 	// Igralna plošèa je velikosti N x N.
 	
 	public static final int N = 19;
-		
+	
+	// število istih krogcev ki jih moramo imeti v vrstici/stolpcu/diagonali da zmagamo.
+	
+	public static final int M = 5;
+	
+	// Atributi ki jih ima igra.
 	
 	private Polje[][] igralna_plošèa;
 	private Igralec na_potezi;
 	
+	// Ustvarimo novo igralno plošèo dimenzije N x N.
+	// z i oznaèimo vrstice, z j pa stolpce.
 	
 	public void zaèetek_igre() {
-		// Ustvarimo novo igralno plošèo dimenzije N x N.
 		igralna_plošèa = new Polje[N][N];
-		// z i oznaèimo vrstice, z j pa stolpce.
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					igralna_plošèa[i][j] = Polje.Prazno;
@@ -22,6 +27,31 @@ public class Igra {
 		na_potezi = Igralec.È;
 	}
 	
+	// Odigramo potezo.
 	
+	public boolean odigrajPotezo(Poteza p) {
+		if (igralna_plošèa[p.getX()][p.getY()] == Polje.Prazno) {
+			igralna_plošèa[p.getX()][p.getY()] = na_potezi.getPolje();
+			
+			if(aliJeKdoZmagal(p)) {
+				System.out.println("GJ zmagu si");
+			}else {
+			na_potezi = na_potezi.nasprotnik();
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	// Preverimo ali je kdo zmagal.
+	
+	public boolean aliJeKdoZmagal(Poteza p) {
+		
+		// definiri smeri v kater je treba pregledat (8 smeri) in se zapelji èez nje in preglej do M (5) koordinat.
+		
+		return false;
+	}
 	
 }
--- Spin/NeumannM16$/_System/Desktop/SnowflipperPenguin/Gomoku/src/Igralec.java
+++ Spin/NeumannM16$/_System/Desktop/SnowflipperPenguin/Gomoku/src/Igralec.java
@@ -4,4 +4,18 @@ public enum Igralec {
 	// Imamo dva igralca èrnega in belega
 	
 	È, B;
+	
+	// Zamenja to kdo je na potezi.
+	
+	public Igralec nasprotnik() {
+		return (this == È ? B : È);
+	}
+	
+	// Vrne polje tistega ki je na potezi.
+
+	public Polje getPolje() {
+		return (this == È ? Polje.B : Polje.È);
+	}
+
+	
 }
